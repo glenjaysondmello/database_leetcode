dff34865c6a5f3b04366a123dd4405d6cdd5d5d6
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-09-03 19:02:51 +0530

    Create s.sql

---
+++ s.sql
@@ -0,0 +1,11 @@
+// MySQL
+  
+DELETE P1 FROM PERSON P1
+JOIN PERSON P2
+ON P1.email = P2.email AND P1.id > P2.id;
+
+// PostgreSQL
+
+DELETE FROM PERSON P1
+USING PERSON P2
+WHERE P1.email = P2.email AND P1.id > P2.id;
