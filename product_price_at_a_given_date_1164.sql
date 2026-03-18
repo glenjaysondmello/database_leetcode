66623ab7afad03c93f4d070617970a9dd0972d92
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2026-03-18 15:19:47 +0530

    Create product_price_at_a_given_date_1164.sql

---
+++ product_price_at_a_given_date_1164.sql
@@ -0,0 +1,38 @@
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+UNION
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+);
+
+-- Example 1:
+
+-- Input: 
+-- Products table:
+-- +------------+-----------+-------------+
+-- | product_id | new_price | change_date |
+-- +------------+-----------+-------------+
+-- | 1          | 20        | 2019-08-14  |
+-- | 2          | 50        | 2019-08-14  |
+-- | 1          | 30        | 2019-08-15  |
+-- | 1          | 35        | 2019-08-16  |
+-- | 2          | 65        | 2019-08-17  |
+-- | 3          | 20        | 2019-08-18  |
+-- +------------+-----------+-------------+
+
+-- Output: 
+-- +------------+-------+
+-- | product_id | price |
+-- +------------+-------+
+-- | 2          | 50    |
+-- | 1          | 35    |
+-- | 3          | 10    |
+-- +------------+-------+
+
