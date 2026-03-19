691990385f051a3a9dc2b2656c3fb82341dce9fb
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2026-03-19 11:13:27 +0530

    Create immediate_food_delivery_ii_1174.sql

---
+++ immediate_food_delivery_ii_1174.sql
@@ -0,0 +1,63 @@
+-- MySQL
+
+-- Uses boolean expression directly in AVG() (treated as 1/0) to calculate the percentage of immediate deliveries.
+SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
+
+-- PostgreSQL
+
+-- Casts boolean to integer using ::INT before averaging, which is required in PostgreSQL.
+SELECT ROUND(AVG((order_date = customer_pref_delivery_date)::INT) * 100, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
+
+-- Uses FILTER with COUNT to explicitly compute the percentage, offering better readability and control.
+SELECT ROUND(
+    (COUNT(*) FILTER (WHERE order_date = customer_pref_delivery_date) * 100::NUMERIC) / COUNT(*), -- can also (... * 100.0) / ...
+2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
+
+-- Example 1:
+
+-- Input: 
+-- Delivery table:
+-- +-------------+-------------+------------+-----------------------------+
+-- | delivery_id | customer_id | order_date | customer_pref_delivery_date |
+-- +-------------+-------------+------------+-----------------------------+
+-- | 1           | 1           | 2019-08-01 | 2019-08-02                  |
+-- | 2           | 2           | 2019-08-02 | 2019-08-02                  |
+-- | 3           | 1           | 2019-08-11 | 2019-08-12                  |
+-- | 4           | 3           | 2019-08-24 | 2019-08-24                  |
+-- | 5           | 3           | 2019-08-21 | 2019-08-22                  |
+-- | 6           | 2           | 2019-08-11 | 2019-08-13                  |
+-- | 7           | 4           | 2019-08-09 | 2019-08-09                  |
+-- +-------------+-------------+------------+-----------------------------+
+
+-- Output: 
+-- +----------------------+
+-- | immediate_percentage |
+-- +----------------------+
+-- | 50.00                |
+-- +----------------------+
+
+-- Explanation: 
+-- The customer id 1 has a first order with delivery id 1 and it is scheduled.
+-- The customer id 2 has a first order with delivery id 2 and it is immediate.
+-- The customer id 3 has a first order with delivery id 5 and it is scheduled.
+-- The customer id 4 has a first order with delivery id 7 and it is immediate.
+-- Hence, half the customers have immediate first orders.
+
